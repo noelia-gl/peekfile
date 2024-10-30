1d064c1a46775b3922117ec9794f8cfebf471526
Noelia <noelia.gil01@estudiant.upf.edu>
2024-10-30 21:39:34 +0100

    count lines updated

--- countlines.sh
+++ countlines.sh
@@ -1,5 +1,4 @@
-file=$1
 
-count=$(wc -l < $file)
+for file in $@; do count=$(wc -l < $file); if [[ $count -eq 0 ]]; then echo The file $file has 0 lines; elif [[ $count -eq 1 ]]; then echo The file $file has 1 line; else echo The file $file has $count lines; fi; done
 
-if [[ $count -eq 0 ]]; then echo The file $file has 0 lines; elif [[ $count -eq 1 ]]; then echo The file $file has 1 line; else echo The file $file has $count lines; fi
+
