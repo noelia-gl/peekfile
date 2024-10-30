42517612e42317634da5072469074fd25b0826a3
Noelia <noelia.gil01@estudiant.upf.edu>
2024-10-30 10:20:18 +0100

    modified_2

--- peek.sh
+++ peek.sh
@@ -1,3 +1,5 @@
-head -n $2 $1
-echo "..."
-tail -n $2 $1
+
+if [[ -z $2 ]]; then X=3; else X=$2; fi
+
+if [[ $(wc -l < $1) -le $((2 * $X)) ]]; then cat $1; else echo ===== Warning: The file $1 contains more than $((2 * $X)) lines; head -n $X $1; echo "..."; tail -n $X $1; fi
+
