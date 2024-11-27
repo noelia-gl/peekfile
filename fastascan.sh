63cfe10fe4fdf23758d0be952163d609a5f58377
Noelia <noelia.gil01@estudiant.upf.edu>
2024-11-27 21:35:17 +0100

    v.01.1

--- fastascan.sh
+++ fastascan.sh
@@ -1,16 +1,17 @@
 #! 
 
 # The default folder X is current folder 
-if [[ $1 -d ]]; then X=.; else X=$1; fi
+if [[ -d $1 ]]; then X=$1; else X=.; fi
 
 # The default number of lines N is 0
 if [[ -z $2 ]]; then N=0; else N=$2; fi
 
 # Number of fasta/fa files in folder X
-FA=$(find . -name "*.fasta" -or -name "*.fa" | wc -l)
-echo There are $FA files in $1 
+FA_N=$(find $1 -name "*.fasta" -or -name "*.fa" | wc -l)
+echo There are $FA_N fasta files in folder $1 
 
 # Number of unique fasta ID in total 
-awk '/>/{print $1}' $FA | sort | uniq | wc -l
-
+FA=$(find $1 -name "*.fasta" -or -name "*.fa")
+ID_uniq=$(awk '/>/{print $1}' $FA | sort | uniq | wc -l)
+echo There are $ID_uniq uniq ID in folder $1
 
