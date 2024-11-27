912e043e896eb7c284dbe863ba6564d40b46a3ee
Noelia <noelia.gil01@estudiant.upf.edu>
2024-11-27 22:09:00 +0100

    v.02

--- fastascan.sh
+++ fastascan.sh
@@ -15,3 +15,21 @@ FA=$(find $1 -name "*.fasta" -or -name "*.fa")
 ID_uniq=$(awk '/>/{print $1}' $FA | sort | uniq | wc -l)
 echo There are $ID_uniq uniq ID in folder $1
 
+# Report of fasta/fa files 
+for i in $FA
+	do echo ==== Report of file $i: ====
+		# Check if the file is a symbolic link 
+		if [[ -h $i ]]; then echo The file $i is a symbolic link
+		else echo The file $i is NOT a symbolic link
+		fi 	
+	# Number of sequences inside each file
+	SEQ=$(grep -c ">" $i)
+		if [[ $SEQ -eq 0 ]]
+		then echo There are NOT sequences inside
+		elif [[ $SEQ -eq 1 ]]
+		then echo There is 1 sequence inside
+		else echo There are $SEQ sequences inside 
+		fi
+	done
+
+
