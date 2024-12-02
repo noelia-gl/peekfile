58abb696c5e4785829f0fd87940bc284dc4f5c55
Noelia <noelia.gil01@estudiant.upf.edu>
2024-12-02 10:54:47 +0100

    Final version

--- fastascan.sh
+++ fastascan.sh
@@ -1,49 +1,72 @@
-#! 
+#!/usr/bin/env bash
 
-# The default folder X is current folder 
-if [[ -d $1 ]]; then X=$1; else X=.; fi
+# Check if the folder $1 exists, is executable and readable. The default folder is current folder (.)
+if [[ -d $1 && -r $1 && -x $1 ]]; then X=$1; else X=.; fi
 
 # The default number of lines N is 0
 if [[ -z $2 ]]; then N=0; else N=$2; fi
 
 # Number of fasta/fa files in folder X
 FA_N=$(find $1 -name "*.fasta" -or -name "*.fa" | wc -l)
-echo There are $FA_N fasta files in folder $1 
+if [[ $FA_N -eq 0 ]]; then 
+	echo No fasta files in folder $1
+else 
+	echo There are $FA_N fasta files in folder $1
+fi
 
 # Number of unique fasta ID in total 
 FA=$(find $1 -type f -name "*.fasta" -or -name "*.fa")
 ID_uniq=$(awk '/>/{print $1}' $FA | sort | uniq | wc -l)
-echo There are $ID_uniq uniq ID in folder $1
+if [[ $ID_uniq -eq 0 ]]; then 
+	echo No unique fasta IDs found
+else 
+	echo There are $ID_uniq uniq ID in folder $1
+fi
 
 # Report of fasta/fa files 
-for i in $FA
-	do echo ==== Report of file: $i ====
-		# Check if the file is a symbolic link 
-		if [[ -h $i ]]
-			then echo The file is a symbolic link
-		else echo The file is NOT a symbolic link
-		fi 
+for i in $FA; do 
+	echo ==== Report of file: $i ====
+	
+	# Check if the file is a symbolic link 
+	if [[ -h $i ]]; then
+		echo The file is a symbolic link
+	else 
+		echo The file is NOT a symbolic link
+	fi 
+	
 	# Check if the files are not empty 
-	if [[ -s $i ]]  
+	if [[ -s $i ]]; then
 		# Number of sequences inside each file
-		then SEQ=$(grep -c ">" $i)
-		if [[ $SEQ -eq 1 ]]
-			then echo There is 1 sequence inside
-			else echo There are $SEQ sequences inside 
+		SEQ=$(grep -c ">" $i)
+		if [[ $SEQ -eq 1 ]]; then
+			echo There is 1 sequence inside
+		else 
+			echo There are $SEQ sequences inside 
 		fi
+		
 		# Total sequence length 
 		awk '!/>/{gsub(/[^A-Za-z]/, "") ##only counts for the sequence itself
 		total_length += length($0)}
 		END{print "Total sequence length: " total_length}' $i
-	else echo File is empty: NO sequences inside
+	
+		#Sequence type depending on content 
+		if grep -v "^>" $i | grep -q "[^ACTGNactgn]"; then
+			seq_type="Amino acid sequence" 
+		else 
+			seq_type="Nucleotide sequence"
+		fi
+		echo Sequence type: $seq_type
+	else 
+		echo File is empty: NO sequences inside
 	fi
+		 
 	# Display full content 
-	if [[ $N -eq 0 ]]; 
+	if [[ $N -eq 0 ]]; then
 		# If $N is 0, skip this part
-		then echo Skip display content because number of lines is 0	
-	elif [[ $(wc -l < $i) -le $((2 * $N)) ]]
+		echo Skip display content because number of lines is 0	
+	elif [[ $(wc -l < $i) -le $((2 * $N)) ]]; then
 		# If file has 2*$N lines or less, display full content
-		then echo Full file content: 
+		echo Full file content: 
 		cat $i
 	else 
 		# If file has more than 2*$N lines, only display first and last $N lines
@@ -52,6 +75,5 @@ for i in $FA
 		echo "..."
 		tail -n $N $i 
 	fi
+	
 done
-
-
